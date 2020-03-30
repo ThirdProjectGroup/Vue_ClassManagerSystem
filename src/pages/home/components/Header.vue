5b13d629af5d1605d55ad3726d5cc4381baea3e0
oushengqiang <1260388182>
2020-03-30 16:31:32 +0800

    初始化vue-cli

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<!--  -->
+<template>
+<div class='header'>
+    this is Managerment header
+</div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+//import Swiper from "swiper";
+
+export default {
+name:'HomeHeader',
+//import引入的组件需要注入到对象中才能使用
+components: {
+    
+},
+data() {
+//这里存放数据
+return {
+
+};
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合 专门存放方法比如点击事件
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+//this.getData();//加载完后执行getData这个函数
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+        fontsize: 100px
+</style>
